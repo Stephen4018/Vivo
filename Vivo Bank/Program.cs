eeee77f90ae58c6eb3820b7006cf6d306239a778
stephen4018 <stephenohaeri4018@gmail.com>
2023-02-11 04:01:05 +0100

    Add project files.

---
+++ Vivo Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("**************VIVO BANK***************");
+        Console.WriteLine("\t\tLogin Page");
+
+        Console.WriteLine("Enter Username");
+        string userName = Console.ReadLine();
+
+       if(userName != "")
+        {
+            Console.WriteLine("Password : use a strong password");
+            string password = Console.ReadLine();
+        }
+
+    }
+}
